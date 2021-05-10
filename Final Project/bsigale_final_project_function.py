5e3c71d8a0ebf8ccb151fb1539b294fa2b5ecc35
Bseagull <bseagullmc6298@gmail.com>
2021-05-09 19:35:31 -0500

    Rev. 2

--- Final Project/bsigale_final_project_function.py
+++ Final Project/bsigale_final_project_function.py
@@ -9,12 +9,11 @@
 #     - Create new database in defined workspace.
 #     - Check input feature data types.
 #     - Reproject any unmatching feature classes to that of the roads input.
-#     - Clip data into the necessary boundaries.
-#         - This is first done by clipping the hwm input to svi, and then doing the reverse using a spatial join on svi.
+#     - Clip point data into the polygon input boundaries.
 #     - Centroids are calculated for the input polygons to estimate distance to roads.
 #     - Intersect analysis is performed between roads and evac inputs to estimate evacuation route access points.
 #     - Distance between polygon centroids and the nearest intersection is calculated.
-#     - Specified summary statistics are computed for the provided inputs.
+#     - Specified summary statistics are computed for the provided inputs, keeping only polygons which intersect with points.
 #     - Data is joined together using the input polygon's unique identifier.
 #     - Data is exported to a feature class, then to a shapefile, using the user-defined output information and field mappings.
 #
@@ -22,8 +21,7 @@
 # Email brendan.sigale@gmail.com with any questions.
 ################################################################################################################################################
 
-def evacuationAccessibilityandPSVI(svi, evac, roads, hwm, workspace, joinField, statField, statType, output_path, output_name, fieldmap = None):
-
+def evacuationAccessibilityandPSVI(svi, evac, roads, hwm, workspace, joinField, statField, statType, output_path, output_name, fieldmap=None):
     # Input definitions:
     # svi - Path to polygons, in this analysis representing census block groups.
     # evac - Path to polylines, in this case representing TxDOT's emergency evacuation routes.
@@ -39,19 +37,18 @@ def evacuationAccessibilityandPSVI(svi, evac, roads, hwm, workspace, joinField, 
     # fieldmap - To export only specific columns, this analysis must essentially be run twice. After the first run, in which a shapefile with all data
     # will be created, the user should use Arc's FeatureClasstoFeatureClass tool to remove unwanted columns. Once this is complete,
     # copy the program's field mappings from the results pane into a fieldmappings string, which can then be passed into the function.
-    
-    
+
     ## intialize useful variables names
-    roads_clip = 'roads_clipped'
     svi_cent = 'svi_centroids'
     intersect = 'intersection_points'
     clipped_svi_hwm = 'svi_clipped_hwm'
     hwm_clip = 'hwm_clipped'
     hwm_summary = 'hwm_summary'
-    
+
+    arcpy.env.overwriteOutput = True
     arcpy.CreateFileGDB_management(workspace, 'SVI_Analysis.gdb')
     arcpy.env.workspace = 'SVI_Analysis.gdb'
-    
+
     ## get spatial references for matching to roads
     ## Check input feature class spatial reference
     roads_desc = arcpy.Describe(roads)
@@ -62,9 +59,7 @@ def evacuationAccessibilityandPSVI(svi, evac, roads, hwm, workspace, joinField, 
     evac_desc_name = evac_desc.spatialReference.name
     hwm_desc = arcpy.Describe(hwm)
     hwm_desc_name = hwm_desc.spatialReference.name
-    damage_desc = arcpy.Describe(damage)
-    damage_desc_name = damage_desc.spatialReference.name
-    
+
     ## Checking shapefile types
     if roads_desc.shapeType != 'Polyline':
         raise TypeError('Roads must be a Polyline!')
@@ -76,12 +71,12 @@ def evacuationAccessibilityandPSVI(svi, evac, roads, hwm, workspace, joinField, 
         raise TypeError('High Water marks must be Points!')
     else:
         print("Input feature class types accepted.")
-    
+
     ## Ensure projections all match, using roads as the defining piece. Will convert SVI to WGS-84 at the end for
-    ## for compatability with Geographic Visualization project. 
+    ## for compatability with Geographic Visualization project.
 
     if roads_desc_name != svi_desc_name:
-        print("Roads and SVI do not match, updating svi in svi_reprojected to match roads: " + roads_desc_name)
+        print("Roads and Evac do not match, updating svi in svi_reprojected to match roads: " + roads_desc_name)
         arcpy.management.Project(svi, 'svi_reprojected', roads_desc.spatialReference)
         svi = arcpy.env.workspace + '/svi_reprojected'
         print("Projection updated, both svi and roads are projected in: " + roads_desc_name)
@@ -103,59 +98,71 @@ def evacuationAccessibilityandPSVI(svi, evac, roads, hwm, workspace, joinField, 
         print("Projection updated, both hwm and roads are projected in: " + roads_desc_name)
     else:
         print("High Water Marks feature class spatial reference matches.")
-        
-        
+
     ## clip high water marks to be within possible svi regions (Must be done first, as the svi is clipped after using these points)
     arcpy.analysis.Clip(hwm, svi, hwm_clip)
     print("Points clipped successfully.")
-    
-    ## spatial join to clip svi down to hwm areas
-    arcpy.analysis.SpatialJoin(svi, hwm_clip, clipped_svi_hwm, join_type = 'KEEP_COMMON',
-                               match_option = 'INTERSECT')
-    print("Polygons clipped successfully.")
-    
+
     ## Convert census block groups to centroids for point to point distance analysis.
-    arcpy.management.FeatureToPoint(clipped_svi_hwm, svi_cent, "CENTROID")
-    
+    arcpy.management.FeatureToPoint(svi, svi_cent, "CENTROID")
+
     ## intersect analysis between roads and evacuation routes
+    print("Calculating evacuation route access points...")
     intersect_features = [roads, evac]
-    arcpy.analysis.Intersect(intersect_features, intersect, 'ONLY_FID', output_type = 'POINT')
+    arcpy.analysis.Intersect(intersect_features, intersect, 'ONLY_FID', output_type='POINT')
     print("Evacuation route entrances generated successfully.")
-    
+
     ## Distance from census block group centroid to nearest road and evacuation route intersection (on ramp estimation)
-    arcpy.analysis.Near(svi_cent, intersect, method = "GEODESIC")
+    arcpy.analysis.Near(svi_cent, intersect, method="GEODESIC")
     print("Near analysis complete.")
-    
+
     field_names_hwm_clip = [f.name for f in arcpy.ListFields(hwm_clip)]
     if statField not in field_names_hwm_clip:
         raise NameError("Input summary statistic not in High Water Marks feature class")
-    
+
+    numFields = []
+    for field in arcpy.ListFields(hwm_clip):
+        if field.type == 'Integer' or field.type == 'Double' or field.type == 'SmallInteger':
+            numFields.append(field.name)
+    if statField not in numFields:
+        raise TypeError("Input summary statistic must be a numeric type.")
+
     statOptions = ['SUM', 'MEAN', 'MIN', 'MAX', 'STDDEV']
     if statType not in statOptions:
         raise NameError("Input summary statistic type not an available option")
-    
+
     try:
         ## Summarize high water mark points into high water summary feature class, summary stat of input variable
-        arcpy.analysis.SummarizeWithin(clipped_svi_hwm, hwm_clip, hwm_summary, 'KEEP_ALL', [[statField, statType]])
+        arcpy.analysis.SummarizeWithin(svi, hwm_clip, hwm_summary, 'ONLY_INTERSECTING', [[statField, statType]])
         print("Summary statistics computer successfully.")
     except:
         print("Could not compute. Likely possibility is statField is not numeric.")
-    
+
     field_names_join = [f.name for f in arcpy.ListFields(hwm_summary)]
     if joinField not in field_names_join:
         raise NameError("Join Field is not in the input SVI feature class")
-    
+
     ## Joining with census block group centroids to add nearest on ramp to table
     arcpy.management.AddJoin(hwm_summary, joinField, svi_cent, joinField)
     print("Join complete.")
-    
+
     if fieldmap is None:
         arcpy.FeatureClassToFeatureClass_conversion(hwm_summary, output_path, output_name)
     else:
         ## Make a feature class with the specified names. This is the default.
-        arcpy.FeatureClassToFeatureClass_conversion(hwm_summary, output_path, output_name, field_mapping = fieldmap)
+        arcpy.FeatureClassToFeatureClass_conversion(hwm_summary, output_path, output_name, field_mapping=fieldmap)
     print("Feature class with specified columns created successfully.")
-    
+
     ## to output to a new, filtered shapefile using the above field mappings.
     arcpy.conversion.FeatureClassToShapefile(output_name, output_path)
     print("Shapefile created successfully.")
+
+    ## delete newly created tables
+    fc_Delete = [hwm_clip, svi_cent, clipped_svi_hwm, intersect, hwm, 'hwm_reprojected', 'svi_reprojected', 'evac_reprojected']
+
+    for fc in fc_Delete:
+        fc_path = os.path.join(arcpy.env.workspace, fc)
+        if arcpy.Exists(fc_path):
+            arcpy.Delete_management(fc_path)
+
+    print("Created analysis layers deleted.")
